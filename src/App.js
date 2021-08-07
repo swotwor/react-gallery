b194d3591cd38d132018de6f54f597704f8eee6d
Swotwor <swotwor@gmail.com>
2021-08-07 17:42:19 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [images, setImages] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	const [term, setTerm] = React.useState('');
+
+	React.useEffect(() => {
+		fetch(
+			`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+			.then((res) => res.json())
+			.then((data) => {
+				setImages(data.hits);
+				setIsLoading(false);
+			})
+			.then((err) => console.log(err));
+	}, [term]);
+
+	return (
+		<div className="container mx-auto">
+			<ImageSearch serchText={(text) => setTerm(text)}/>
+
+			{!isLoading && images.length === 0 && <h1 className="text-6xl text-center mx-auto mt-32">No Images Found</h1>}
+
+			{isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+				{images.map(image => (
+					<ImageCard key={image.id} image={image}/>
+				))}
+			</div>}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ImageSearch.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ImageSearch = ({ serchText }) => {
+	const [text, setText] = React.useState('');
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		serchText(text);
+	}
+
+	return (
+		<div className='max-w-sm rounded overflow-hidden my-10 mx-auto'>
+			<form className='w-full max-w-sm' onSubmit={onSubmit}>
+				<div className='flex items-center border-b border-b-2 border-teal-500 py-2'>
+					<input
+						className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
+						type='text'
+						placeholder='Search Image Term...'
+						onChange={(e) => setText(e.target.value)}
+					/>
+					<button
+						className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 py-1 px-2 text-white rounded'
+						type='submit'
+					>
+						Search
+					</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default ImageSearch;
